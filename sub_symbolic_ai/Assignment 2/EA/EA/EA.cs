f1ef40bbf6af54f3a4b20d3b029bc295af6a4278
Johannes <johannbm@stud.ntnu.no>
2016-02-10 21:34:32 +0100

    Created EA skeleton loop and defined roughly interfaces for genotype, phenotype and fitness

---
+++ sub_symbolic_ai/Assignment 2/EA/EA/EA.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA
+{
+    class EA
+    {
+
+        private int maxNumOfGenerations;
+        private double maxFitness;
+
+        private IFitnessCalculator fitnessCalculator;
+        //private IPhenotype phenotype;
+        //private IGenotype genotype;
+
+        public void Run()
+        {
+            //Initialize random population (Genotypes)
+            int numOfGenerations = 0;
+            int bestFitness = 0;
+
+            List<IPhenotype> phenotypes = new List<IPhenotype>();
+            List<IGenotype> genotypes = new List<IGenotype>();
+
+            while (numOfGenerations < maxNumOfGenerations || bestFitness < maxFitness)
+            {
+                //Develop (Genotypes -> Phenotypes)
+
+                //Fitness testing
+                foreach (IPhenotype phenotype in phenotypes)
+                {
+                    fitnessCalculator.CalculateFitness(phenotype);
+                }
+
+                //Adult selection
+
+                //Parent selection
+
+                //Mutation
+
+
+                numOfGenerations++;
+            }
+        } 
+
+
+    }
+}
---
+++ sub_symbolic_ai/Assignment 2/EA/EA/IFitnessCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA
+{
+    interface IFitnessCalculator
+    {
+        double CalculateFitness(IPhenotype phenotype);
+        void CalculateAndSetFitness(IPhenotype phenotype);
+        void CalculateAndSetFitness(IPhenotype[] phenotype);
+    }
+}
