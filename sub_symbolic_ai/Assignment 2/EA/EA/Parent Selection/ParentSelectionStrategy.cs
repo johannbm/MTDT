c2deb8b909242829ef273d5d8940f4fdb2e508b9
Johannes <johannbm@stud.ntnu.no>
2016-02-14 15:47:30 +0100

    Implemented basic statistical methods

--- sub_symbolic_ai/Assignment 2/EA/EA/Parent Selection/ParentSelectionStrategy.cs
+++ sub_symbolic_ai/Assignment 2/EA/EA/Parent Selection/ParentSelectionStrategy.cs
@@ -8,9 +8,34 @@ namespace EA
 {
     public abstract class ParentSelectionStrategy
     {
+
+
         protected double GetFitnessSum(List<IPhenotype> phenotypes)
         {
             return phenotypes.Sum(x => x.fitness);
+        }
+
+        protected double GetFitnessAverage(List<IPhenotype> phenotypes)
+        {
+            return GetFitnessSum(phenotypes) / phenotypes.Count;
+        }
+
+        protected double GetVariance(List<IPhenotype> phenotypes)
+        {
+            double squaredDifferenceSum = 0;
+            double mean = GetFitnessAverage(phenotypes);
+
+            foreach (IPhenotype p in phenotypes)
+            {
+                squaredDifferenceSum += Math.Pow(p.fitness - mean, 2);
+            }
+
+            return squaredDifferenceSum / phenotypes.Count;
+        }
+
+        protected double GetStandardDeviation(List<IPhenotype> phenotypes)
+        {
+            return Math.Sqrt(GetVariance(phenotypes));
         }
 
         public abstract List<IPhenotype> selectParents(List<IPhenotype> candidates, int numberOfParents);
